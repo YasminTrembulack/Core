16140c88dfe8757ed54c7cb8e93cf5d011345bb7
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-15 16:23:05 -0300

    include health_route

---
+++ routes/health_route.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends
+from requests import Session
+from sqlalchemy import text
+
+from database.connection import get_db
+
+router = APIRouter()
+
+
+@router.get("/health")
+def health_check(db: Session = Depends(get_db)):
+    try:
+
+        db.execute(text("SELECT 1"))
+
+        return {
+            "status": "healthy",
+            "database": "connected"
+        }
+
+    except Exception as e:
+
+        return {
+            "status": "unhealthy",
+            "database": "disconnected",
+            "error": str(e)
+        }
+
+    finally:
+        db.close()
