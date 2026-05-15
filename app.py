d79d09dc630afdf80673e5e6d4227e44ff86eba2
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-15 16:47:42 -0300

    fix: app

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from routes.user_route import router as user_route
 from routes.health_route import router as health_route
+from config import settings
 
 app = FastAPI()
 
 app.include_router(user_route)
 app.include_router(health_route)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(settings.PORT)
+    must_reload = settings.ENV == "development"
+
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port,
+        reload=must_reload,
+    )
--- config.py
+++ config.py
@@ -7,5 +7,7 @@ load_dotenv()
 class Settings:
     EXTERNAL_API_URL = os.environ["EXTERNAL_API_URL"]
     DATABASE_URL = os.environ["DATABASE_URL"]
+    PORT = os.getenv("PORT", 8080)
+    ENV = os.environ.get("ENV", "production")
 
 settings = Settings()
