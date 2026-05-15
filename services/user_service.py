feb638d7c941f9917ff71855db86fa3cdd54f468
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-15 16:58:20 -0300

    fix: user ids

--- services/user_service.py
+++ services/user_service.py
@@ -3,12 +3,14 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 
 from repository.user_repository import UserRepository
+from services.galileo_service import GalileoService
 
 
 class UserService:
 
     def __init__(self, db: Session):
         self.repository = UserRepository(db)
+        self.galileo_service = GalileoService()
 
     def create_user_if_not_exists(self, phone: str, first_name: str | None = None, last_name: str | None = None):
         existing_user = self.repository.find_by_phone(phone)
@@ -26,4 +28,8 @@ class UserService:
         return self.repository.add_unit_to_user(user_id=user_id, unit_id=unit_id)
 
     def get_units_from_user(self, user_id: UUID):
-        return self.repository.get_user_units(user_id=user_id)
+        units = self.repository.get_user_units(user_id=user_id)
+        
+        units_ids = set([u['id'] for u in units])
+        
+        return self.galileo_service.get_unit_by_ids(units_ids)
