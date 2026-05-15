b74e8a83dd5464fd2991852a2887a0d00c1a23dc
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-15 17:05:32 -0300

    feat: add loggers

--- services/galileo_service.py
+++ services/galileo_service.py
@@ -1,5 +1,7 @@
 from typing import Set
 
+from fastapi.logger import logger
+
 from config import settings
 from utils.http_client import get_request
 
@@ -8,9 +10,12 @@ class GalileoService:
     
     def get_unit_by_ids(self, ids: Set[str]):
         url = f"{settings.EXTERNAL_API_URL}?route=unidades"
-
+        logger.info(f"User unit ids: {ids}")
+        
         units = get_request(url)
 
+        logger.info(f"User unit ids: {units[:10]}")
+        
         return [
             unit
             for unit in units
